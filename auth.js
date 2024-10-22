ec99fe29785022a27b90f81b52a18d694cc68dea
Taylor Johnson <johnsoctbus@gmail.com>
2024-10-21 20:43:17 -0400

    Added error messaging for the local strategy

--- auth.js
+++ auth.js
@@ -9,9 +9,18 @@ function authentication (db) {
         passport.use(new LocalStrategy((username, password, done) => {
                 db.findOne({ username }, (err, user) => {
                         console.log(`User ${username} attempted to log in.`)
-                        if (err) return done(err)
-                        if (!user) return done(null, false)
-                        if (!bcrypt.compareSync(password, user.password)) return done(null, false)
+                        if (err) {
+                                console.log(err)
+                                return done(err)
+                        }
+                        if (!user) {
+                                console.log('Cannot find user')
+                                return done(null, false)
+                        }
+                        if (!bcrypt.compareSync(password, user.password)) {
+                                console.log('Bad password')
+                                return done(null, false)
+                        }
                         return done(null, user)
                 })
         }))
